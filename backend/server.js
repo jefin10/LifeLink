885918ffbaeac6d235a09aaf2ad550fb6d6ee972
Jefin Francis <147376215+jefin10@users.noreply.github.com>
2025-05-26 02:39:30 +0530

    many changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:3000" , 'http://localhost:5174'],
+    origin: ["http://localhost:5173", "http://localhost:3000" , 'http://localhost:5174','*'],
     credentials: true,
 };
 
